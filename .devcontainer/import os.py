2559629582ddeb1ad7e42223c5be1868be9d7d24
SavvasH-hub <149665191+SavvasH-hub@users.noreply.github.com>
2023-12-03 22:04:32 +1100

    savvasEMP

--- .devcontainer/import os.py
+++ .devcontainer/import os.py
@@ -1,9 +1,20 @@
-import os
+{% load impimport os
 import shutil
 
-# Get the list of all codespaces
-codespaces = os.listdir("codespaces")
+codespaces = [entry.name for entry in os.scandir("codespaces") if entry.is_dir()]
+
 
 # For each codespace, copy its contents into the current codespace
 for codespace in codespaces:
-  shutil.copytree(os.path.join("codespaces", codespace), os.path.join(".", codespace))
+    # Check if the destination directory already exists to avoid error
+    dest_dir = os.path.join(".", codespace)
+    if not os.path.exists(dest_dir):
+        shutil.copytree(os.path.join("codespaces", codespace), dest_dir)
+    else:
+        print(f"Directory '{dest_dir}' already exists.")
+
+# Iterate over all the files in the E:\ drive and print their paths
+for root, dirs, files in os.walk("E:\"):
+    for file in files:
+        print(os.path.join(root, file))
+_tags %}
