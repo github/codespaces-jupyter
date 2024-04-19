845fea145cc2b88d2110b29b2cd9d4f7181acc44
yashwanth <153694555+Fzmine@users.noreply.github.com>
2024-04-19 14:43:09 +0530

    Create function1.py

---
+++ function1.py
@@ -0,0 +1,13 @@
+n=5
+def factorial():
+    fact=1
+    for i in range(1,n):
+      fact=fact+1
+print(fact)
+sum=n*(n+1)/2
+print(sum)
+def sum_of_2_num():
+  a=3
+  b=6
+  print(a+b)
+sum_of_2_num()
