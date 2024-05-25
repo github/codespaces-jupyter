2dc60e7353931f608c60e088fa11123ee66d07c5
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-05-25 07:31:12 +0000

    add dataset to streamlit

--- workshop/streamlit_ui.py
+++ workshop/streamlit_ui.py
@@ -1,6 +1,12 @@
 
 import streamlit as st
+from datasets import load_dataset
+
+st.text("Workshop")
 
 
+dataset = load_dataset(
+"yelp_review_full", split='test'
+)
 
-st.text("Test :)")
+st.dataframe(dataset.to_pandas())
