2bcf3e4f6f5fecbfcce0d0f96d1cfa7f3aebfadc
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-05-30 20:19:49 +0000

    Add example model

--- workshop/streamlit_ui.py
+++ workshop/streamlit_ui.py
@@ -1,6 +1,14 @@
 
 import streamlit as st
-from datasets import load_dataset
+import mlflow
+from sentence_transformers import SentenceTransformer
+sentences = ["This is an example sentence", "Each sentence is converted"]
 
-st.text("Workshop")
+embeddings_model = SentenceTransformer('sentence-transformers/all-mpnet-base-v2')
+model = mlflow.sklearn.load_model("file:///workspaces/build-your-first-ml-pipeline-workshop/notebooks/mlruns/0/bcd3e16c12ac48b48a42b0ebc6e887b9/artifacts/mymodel")
 
+st.title("Workshop")
+
+content= st.text_input("Type your query", value='I lost my card')
+if st.button("Predict"):
+    st.text(model.predict(embeddings_model.encode(content).reshape(1, -1)))
