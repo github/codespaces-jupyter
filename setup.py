2feb34f8f2609427c710b03084ab43e09880b1f4
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-05-31 11:40:49 +0000

    add pipeline to repo

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="workshop",
+    version="0.1.1",
+    packages=[
+        "workshop",
+    ],
+    author="Jean Carlo Machado",
+    author_email="machado.c.jean@gmail.com",
+    description="workshop",
+    long_description="",
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+)
